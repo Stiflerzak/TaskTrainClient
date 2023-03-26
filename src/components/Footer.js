7b5528ec0f4631341abed9106c6775fc2ae9d6af
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-26 20:56:26 +0300

    Added styling to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #00003B;
   height: 80px;
-`;
-
-const Text = styled.p`
   color: #fff;
 `;
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <FooterContainer>
-      <Text>This is the footer</Text>
+      <p>&copy; {currentYear} Your Company Name. All rights reserved.</p>
     </FooterContainer>
   );
 };
